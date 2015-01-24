444c8cfd139cd415911d5da348eadf3262163582
Kei Yasuda <to.yasuda.kei@gmail.com>
2015-01-25 01:17:35 +0900

    update 1.6.4.py

--- ex/ppl/1.6.4.py
+++ ex/ppl/1.6.4.py
@@ -1,24 +1,98 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""0からn-1までの間のk個の重複のない整数列を生成する"""
+"""0からn-1までの間のk個の重複のない整数の乱数列を生成する"""
 
+import sys
+import getopt
 import random
 
-def make_rseq(n, k):
-	seq = []
+def make_randseq1(n, k):
+	"""乱数列を生成　バージョン1"""
+	randseq = []
 
 	for i in range(k):
 		rand = random.randint(0, n - 1)
-		while rand in seq:
+		while rand in randseq:
 			rand = random.randint(0, n - 1)
-		seq.append(rand)
+		randseq.append(rand)
 
-	return seq
+	return randseq
+
+def make_randseq2(n, k):
+	"""乱数列を生成　バージョン2"""
+	randseq = []
+	bucket = [0] * n
+
+	for i in range(k):
+		rand = random.randint(0, n - 1)
+		while bucket[rand] == 1:
+			rand = random.randint(0, n - 1)
+		bucket[rand] = 1
+		randseq.append(rand)
+
+	return randseq
+
+def make_randseq3(n, k):
+	"""乱数列を生成　バージョン3"""
+	randseq = []
+	seq = [0] * n
+
+	for i in range(n):
+		seq[i] = i
+	for i in range(k):
+		rand = random.randint(0, len(seq) - 1)
+		randseq.append(seq.pop(rand))
+
+	return randseq
+
+class Config:
+	"""設定の抽象"""
+	def __init__(self, n=10, k=5, m=make_randseq1, q=False):
+		self.n = n
+		self.k = k
+		self.m = m
+		self.q = q
+
+def parse_args():
+	"""コマンドライン引数を解析して、Configをreturnする"""
+	config = Config()
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "n:k:m:q")
+	except getopt.GetoptError, err:
+		print str(err)
+		sys.exit(1)
+
+	for o, a in opts:
+		if o == "-n":
+			config.n = int(a)
+		elif o == "-k":
+			config.k = int(a)
+		elif o == "-m":
+			if a == "1":
+				config.m = make_randseq1
+			elif a == "2":
+				config.m = make_randseq2
+			elif a == "3":
+				config.m = make_randseq3
+			else:
+				assert False, "invalid option"
+		elif o == "-q":
+			config.q = True
+		else:
+			assert False, "unhandled option"
+
+	return config
+
+def main():
+	config = parse_args()
+
+	for i in config.m(config.n, config.k):
+		if not config.q:
+			print i
 
 if __name__ == '__main__':
-	n = 10
-	k = n/2
-	for i in make_rseq(n, k):
-		print i
+	main()
+
 
