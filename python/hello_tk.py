7c920375f36280ec8a2f0b8ca2108fd8b5dde05e
Kei Yasuda <to.yasuda.kei@gmail.com>
2015-03-24 23:19:11 +0900

    add hello_tk.py

---
+++ python/hello_tk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- config: utf-8 -*-
+
+# refer: http://docs.python.jp/2/library/tkinter.html
+
+from Tkinter import *
+
+class Application(Frame):
+    def say_hi(self):
+        print "hi threre, everyone!"
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT['text'] = 'QUIT'
+        self.QUIT['fg'] = 'red'
+        self.QUIT['command'] = self.quit
+
+        self.QUIT.pack({'side': 'left'})
+
+        self.hi_there = Button(self)
+        self.hi_there['text'] = 'Hello'
+        self.hi_there['command'] = self.say_hi
+
+        self.hi_there.pack({'side' : 'left'})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
