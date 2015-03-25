8264ed9fecccb285c50fcc98e6a21cc8d9ebeb22
Kei Yasuda <to.yasuda.kei@gmail.com>
2015-03-26 08:31:38 +0900

    add p001

--- ex/nlp100_2015/answer.py
+++ ex/nlp100_2015/answer.py
@@ -3,6 +3,7 @@
 
 import sys
 import argparse
+import codecs
 
 def p000():
     """
@@ -11,8 +12,16 @@ def p000():
     """
     print 'stressed'[::-1]
 
+def p001():
+    """
+    01. 「パタトクカシーー」
+    「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
+    """
+    sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+    print u"パタトクカシーー"[::2]
+
 def main():
-    default_number = 0
+    default_number = 1
     parser = argparse.ArgumentParser(description='言語処理100本ノック2015')
     parser.add_argument('-n', '--number', nargs='?', default=default_number, type=int, help='問題の番号')
     args = parser.parse_args()
