7ab3abf28b84389c4dbab9d46fff61fa6bbb3220
Kei Yasuda <to.yasuda.kei@gmail.com>
2015-03-27 22:51:56 +0900

    add p002 and p003

--- ex/nlp100_2015/answer.py
+++ ex/nlp100_2015/answer.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import string
 import argparse
 import codecs
+from collections import Counter
+from collections import OrderedDict
 
 def p000():
     """
@@ -20,8 +23,37 @@ def p001():
     sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
     print u"パタトクカシーー"[::2]
 
+def p002():
+    """
+    02. 「パトカー」＋「タクシー」＝「パタトクカシーー」
+    「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+    """
+    sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+    x = u'パトカー'
+    y = u'タクシー'
+    z = u''
+    for i in range(0, max(len(x), len(y))):
+        z += x[i] + y[i]    # FIXME: might cause IndexError
+    print z
+
+def p003():
+    """
+    03. 円周率
+    "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+    """
+    sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+    sentence = string.lower(sentence)
+    chars = [sentence[i:i+1] for i in range(0, len(sentence))]
+    alphabets = [char for char in chars if char in string.lowercase]
+    counter = Counter()
+    for alpha in alphabets:
+        counter[alpha] += 1
+    ordered = OrderedDict(sorted(counter.items(), key=lambda t: t[1], reverse=True))
+    for key, value in ordered.items():
+        print str(key) + ', ' + str(value)
+
 def main():
-    default_number = 1
+    default_number = 3
     parser = argparse.ArgumentParser(description='言語処理100本ノック2015')
     parser.add_argument('-n', '--number', nargs='?', default=default_number, type=int, help='問題の番号')
     args = parser.parse_args()
@@ -32,3 +64,26 @@ def main():
 if __name__ == '__main__':
     main()
 
+"""
+04. 元素記号
+"Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
+
+05. n-gram
+与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
+
+06. 集合
+"paraparaparadise"と"paragraph"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
+
+07. テンプレートによる文生成
+引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y="気温", z=22.4として，実行結果を確認せよ．
+
+08. 暗号文
+与えられた文字列の各文字を，以下の仕様で変換する関数cipherを実装せよ．
+
+英小文字ならば(219 - 文字コード)の文字に置換
+その他の文字はそのまま出力
+この関数を用い，英語のメッセージを暗号化・復号化せよ．
+
+09. Typoglycemia
+スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ．
+"""
