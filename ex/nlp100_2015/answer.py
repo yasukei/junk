9ded5f2b115a952599963ef8811cdaa48c8e66e0
Kei Yasuda <to.yasuda.kei@gmail.com>
2015-04-23 23:26:48 +0900

    余計な行を削除

--- ex/nlp100_2015/answer.py
+++ ex/nlp100_2015/answer.py
@@ -391,7 +391,6 @@ Wikipediaの記事を以下のフォーマットで書き出したファイルja
 Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
 """
 def p020():
-    decoder = json.JSONDecoder()
     with open('jawiki-country.json', 'r') as f:
         for j in parse_json_string(f.read()):
             if j['title'] == u'イギリス':
