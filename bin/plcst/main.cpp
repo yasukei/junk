f878afc8a3bbaee720f3ad9f7cee09bcbe07fe81
yasukei <yasu_kei@icloud.com>
2018-06-13 23:45:44 +0900

    can parse "PROGRAM a1a END_PROGRAM"

--- bin/plcst/main.cpp
+++ bin/plcst/main.cpp
@@ -6,14 +6,26 @@
 
 int main(int ac, const char* av[])
 {
-	std::cout << "hello" << std::endl;
+	//std::cout << "hello" << std::endl;
 
-	std::string str("3.8,\n3");
-	double d;
+	//std::string str("3.8,\n3");
+	//double d;
 
-	if(parse_numbers(str.begin(), str.end(), d))
+	typedef std::string::const_iterator iterator_type;
+	typedef client::program_parser<iterator_type> program_parser;
+
+	program_parser g; // our grammar
+	std::string str("PROGRAM a1a END_PROGRAM");
+
+	client::program prg;
+	std::string::const_iterator start = str.begin();
+	std::string::const_iterator end = str.end();
+
+	bool r = phrase_parse(start, end, g, boost::spirit::ascii::space, prg);
+	if(r && start == end)
 	{
 		std::cout << "succeeded\n";
+		std::cout << boost::fusion::as_vector(prg) << std::endl;
 	}
 	else
 	{
--- bin/plcst/parser.hpp
+++ bin/plcst/parser.hpp
@@ -4,7 +4,10 @@
 #include <string>
 #include <vector>
 
+#define BOOST_SPIRIT_DEBUG
+
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 
 #include "iec.hpp"
 #include "parser.hpp"
@@ -71,4 +74,71 @@ bool adder(Iterator first, Iterator last, double& n)
     return r;
 }
 
+#include <boost/fusion/include/adapt_struct.hpp>
+
+namespace client
+{
+	struct program
+	{
+		std::string name;
+	};
+}
+
+BOOST_FUSION_ADAPT_STRUCT(
+	client::program,
+	(std::string, name)
+)
+
+namespace client
+{
+	template <typename Iterator>
+	struct program_parser : qi::grammar<Iterator, program(), ascii::space_type>
+	{
+		program_parser() : program_parser::base_type(start)
+		{
+			using qi::lit;
+			using qi::int_;
+			using qi::double_;
+			using qi::lexeme;
+			using qi::on_error;
+			using qi::fail;
+			using qi::retry;
+			using qi::accept;
+			using qi::rethrow;
+			using ascii::char_;
+			using ascii::alpha;
+			using ascii::alnum;
+			using ascii::space;
+			using namespace qi::labels;
+
+			using phoenix::construct;
+			using phoenix::val;
+
+			start %=
+				lit("PROGRAM")
+				//>> *(char_)
+				//>> lexeme["aaa"]						// success
+				//>> lexeme[char_ >> char_ >> char_]	// success
+				> lexeme[(alpha) > *(alnum)]	// success
+				> lexeme["END_PROGRAM"]
+				;
+
+			//start.name("start");
+
+			on_error<fail>
+				(
+				 start,
+				 std::cout
+				 	<< val("Error! Expecting ")
+					<< _4
+					<< val(" here: \"")
+					<< construct<std::string>(_3, _2)
+					<< val("\"")
+					<< std::endl
+				);
+		}
+
+		qi::rule<Iterator, program(), ascii::space_type> start;
+	};
+}
 
