704b41f47c4e75822b491914d9e0f3de1a479622
Kei Yasuda <adieuyaks@gmail.com>
2021-07-27 00:58:31 +0900

    made test_mycat.py parameterized

--- python/test_mycat.py
+++ python/test_mycat.py
@@ -4,15 +4,26 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 
-
-class TestMyCat(unittest.TestCase):
-            
+class TestMyCat:
     @patch('sys.stdout', new_callable=StringIO)
-    def test_cat_two_files(self, mock_stdout):
-        files = [StringIO('a'), StringIO('b')]
-        mycat.cat(files, False)
-        self.assertEqual(mock_stdout.getvalue(), 'a\nb\n')
-        
+    def test_mycat(self, mock_stdout):
+        mycat.cat(self.args['files'], self.args['n'])
+        self.assertEqual(mock_stdout.getvalue(), self.args['expect'])
+
+
+class TestMyCat1(TestMyCat, unittest.TestCase):
+    args = {
+        'files' : [StringIO('')],
+        'n' : False,
+        'expect' : '\n',
+        }        
+
+class TestMyCat2(TestMyCat, unittest.TestCase):
+    args = {
+        'files' : [StringIO('a'), StringIO('b')],
+        'n' : False,
+        'expect' : 'a\nb\n',
+        }        
 
 if __name__ == "__main__":
     unittest.main()
