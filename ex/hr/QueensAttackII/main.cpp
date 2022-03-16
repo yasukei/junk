ee646d3b957fd18212c50f4cc052383261297fe7
Kei Yasuda <adieuyaks@gmail.com>
2022-03-16 20:05:58 +0900

    updated

--- ex/hr/QueensAttackII/main.cpp
+++ ex/hr/QueensAttackII/main.cpp
@@ -1,13 +1,5 @@
 #include <bits/stdc++.h>
 
-/*
-	- multi-threading
-	- convert values in S into indirect index
-
- */
-
-#include <bits/stdc++.h>
-
 #define printf
 
 int main(void)
@@ -17,101 +9,135 @@ int main(void)
     std::cin >> n >> k;    
     std::cin >> rq >> cq;
 
-    std::vector<std::vector<bool>> obstacles;
+    std::vector<std::bitset<100*1000>> obstacles;
     obstacles.resize(n+1);
-    for(int i = 0; i <= n; ++i)
-    {
-        obstacles[i].resize(n+1);    
-    }
+    //for(int i = 0; i <= n; ++i)
+    //{
+    //    obstacles[i].resize(n+1);    
+    //}
     int r, c;
     for(int i = 0; i < k; ++i)
     {
         std::cin >> r >> c;
-        obstacles[r][c] = true;
+        //obstacles[r][c] = true;
+        obstacles[r].set(c);
     }
 
-    int numofCanAttack = 0;
+	std::atomic<int> numofCanAttack(0);
+
     // Up
-    for(int i = rq + 1, j = cq; i <= n; ++i)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tUp([&](){
+		for(int i = rq + 1, j = cq; i <= n; ++i)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
     // Down
-    for(int i = rq - 1, j = cq; i >= 1; --i)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tDown([&](){
+		for(int i = rq - 1, j = cq; i >= 1; --i)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
     // Right
-    for(int i = rq, j = cq + 1; j <= n; ++j)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tRight([&](){
+		for(int i = rq, j = cq + 1; j <= n; ++j)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
     // Left
-    for(int i = rq, j = cq - 1; j >= 1; --j)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tLeft([&](){
+		for(int i = rq, j = cq - 1; j >= 1; --j)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
 
     // Right-Up
-    for(int i = rq + 1, j = cq + 1; i <= n && j <= n; ++i, ++j)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tRightUp([&](){
+		for(int i = rq + 1, j = cq + 1; i <= n && j <= n; ++i, ++j)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
+	tUp.join();
+	tDown.join();
+	tRight.join();
+	tLeft.join();
+
     // Right-Down
-    for(int i = rq + 1, j = cq - 1; i <= n && j >= 1; ++i, --j)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tRightDown([&](){
+		for(int i = rq + 1, j = cq - 1; i <= n && j >= 1; ++i, --j)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
     // Left-Down
-    for(int i = rq - 1, j = cq - 1; i >= 1 && j >= 1; --i, --j)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tLeftDown([&](){
+		for(int i = rq - 1, j = cq - 1; i >= 1 && j >= 1; --i, --j)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
     // Left-Up
-    for(int i = rq - 1, j = cq + 1; i >= 1 && j <= n; --i, ++j)
-    {
-        if(obstacles[i][j])
-        {
-            break;
-        }
-        printf("i: [%d], j: [%d]\n", i, j);
-        numofCanAttack++;
-    }
+	std::thread tLeftUp([&](){
+		for(int i = rq - 1, j = cq + 1; i >= 1 && j <= n; --i, ++j)
+		{
+			if(obstacles[i][j])
+			{
+				break;
+			}
+			printf("i: [%d], j: [%d]\n", i, j);
+			numofCanAttack++;
+		}
+	});
+
+	tRightUp.join();
+	tRightDown.join();
+	tLeftDown.join();
+	tLeftUp.join();
 
     std::cout << numofCanAttack << std::endl;
 
