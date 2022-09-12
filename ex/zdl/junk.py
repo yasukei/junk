3cdf73fc228b9563797e34c2762c9db805a60557
Kei Yasuda <adieuyaks@gmail.com>
2022-09-12 12:01:36 +0900

    added ex/zdl

---
+++ ex/zdl/junk.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp > 0:
+        return 1
+    return 0
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp > 0:
+        return 1
+    return 0
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp > 0:
+        return 1
+    return 0
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    return AND(s1, s2)
