a0ef3a571e5b61481768b4254e58ae002c889718
Kei Yasuda <yasu_kei@icloud.com>
2023-01-06 15:05:17 +0900

    updated

--- ex/atcoder/hokudai-hitachi2022_a/main.py
+++ ex/atcoder/hokudai-hitachi2022_a/main.py
@@ -147,68 +147,219 @@ class GridGraph:
         #        # TODO
         #        pass
 
-        result = False
         for vertex in self._4connectionVertex:
+            result = False
             for edge in self._connectedEdges[vertex]:
-                result = self._exploreForOrigin(edge.v(), edge.d(), 0, 1, [vertex])
+                result = self._findClosedLoop(edge.v(), edge.d(), 0, 1, [vertex], self._foundCallbackForOrigin)
                 if result:
                     break
             if result:
                 break
+
+        queue = collections.deque()
+        memo = set()
+        for x, y in self._position.values():
+            vertex = self._grid[x][y]
+            queue.append(vertex)
+            memo.add(vertex)
+        def _foundCallbackWithEnqueAndMemo(edge_length, trajectory):
+            self._foundCallback(edge_length, trajectory)
+            for vertex in trajectory:
+                if vertex in memo:
+                    continue
+                queue.append(vertex)
+                memo.add(vertex)
+
+        while len(queue) > 0:
+        #for _ in range(4):
+            vertex = queue.popleft()
+            for edge1 in self._connectedEdges[vertex]:
+                if self._position.get(edge1.v()) == None:
+                    continue
+
+                for edge2 in self._connectedEdges[edge1.v()]:
+                    result = self._findClosedLoop(edge2.v(), edge1.d(), 0, 2, [vertex, edge1.v()], _foundCallbackWithEnqueAndMemo)
+
+            if len(queue) == 0:
+                for vertex in self._4connectionVertex:
+                    if self._position.get(vertex) == None:
+                        continue
+
+                    fixed = 0
+                    for edge in self._connectedEdges[vertex]:
+                        if self._position.get(edge.v()) != None:
+                            fixed += 1
+                    if fixed != 3:
+                        continue
+
+                    undetermined_edge = None
+                    determined_direction = set()
+                    for edge in self._connectedEdges[vertex]:
+                        if self._position.get(edge.v()) == None:
+                            undetermined_edge = edge
+                        else:
+                            direction = self._getDirection(vertex, edge.v())
+                            determined_direction.add(direction)
+
+                    x, y = self._position[vertex]
+                    if Direction.UP not in determined_direction:
+                        self._addVertexToGrid(undetermined_edge.v(), x, y + undetermined_edge.d())
+                    elif Direction.DOWN not in determined_direction:
+                        self._addVertexToGrid(undetermined_edge.v(), x, y - undetermined_edge.d())
+                    elif Direction.RIGHT not in determined_direction:
+                        self._addVertexToGrid(undetermined_edge.v(), x + undetermined_edge.d(), y)
+                    else: # Direction.LEFT not in determined_direction:
+                        self._addVertexToGrid(undetermined_edge.v(), x - undetermined_edge.d(), y)
+                    queue.append(undetermined_edge.v())
+                    memo.add(undetermined_edge.v())
+
         self.show()
 
-        # TODO
 
-    def _exploreForOrigin(self, vertex, edge_length, current_length, depth, trajectory):
+    def _foundCallbackForOrigin(self, edge_length, trajectory):
+        index = 1
+
+        # add vertex on 1st edge
+        x = 0
+        y = 0
+        index = self._addVerticesOnEdgeRightward(x, y, edge_length, trajectory, index)
+
+        # add vertex on 2nd edge
+        x = edge_length
+        y = 0
+        index = self._addVerticesOnEdgeUpward(x, y, edge_length, trajectory, index)
+
+        # add vertex on 3rd edge
+        x = edge_length
+        y = edge_length
+        index = self._addVerticesOnEdgeLeftward(x, y, edge_length, trajectory, index)
+
+        # add vertex on 4th edge
+        x = edge_length
+        y = 0
+        index = self._addVerticesOnEdgeLeftward(x, y, edge_length, trajectory, index)
+
+    def _foundCallback(self, edge_length, trajectory):
+
+        vertex1 = trajectory[0] # 1st edge start vertex
+        vertex2 = trajectory[1] # 1st edge end vertex & 2nd edge start vertex
+        vertex3 = trajectory[2] # vertex on 2nd edge, or 2nd edge end vertex & 3rd edge start vertex
+        directionof_1st_edge = self._getDirection(vertex1, vertex2)
+        directionof_2nd_edge = None
+        if self._position.get(vertex3) != None:
+            directionof_2nd_edge = self._getDirection(vertex2, vertex3)
+        else:
+            for edge in self._connectedEdges[vertex2]:
+                if self._position.get(edge.v()) == None:
+                    continue
+                direction = self._getDirection(vertex2, edge.v())
+                if directionof_1st_edge in (Direction.UP, Direction.DOWN) and direction in (Direction.UP, Direction.DOWN):
+                    continue
+                if directionof_1st_edge in (Direction.RIGHT, Direction.LEFT) and direction in (Direction.RIGHT, Direction.LEFT):
+                    continue
+
+                if direction == Direction.UP:
+                    directionof_2nd_edge = Direction.DOWN
+                elif direction == Direction.DOWN:
+                    directionof_2nd_edge = Direction.UP
+                elif direction == Direction.RIGHT:
+                    directionof_2nd_edge = Direction.LEFT
+                else: # direction == Direction.LEFT:
+                    directionof_2nd_edge = Direction.RIGHT
+                break
+        log.debug('dir1={}, dir2={}'.format(directionof_1st_edge, directionof_2nd_edge))
+
+        index = 2
+        x, y = self._position[vertex2]
+
+        # add vertex on 2nd edge
+        if directionof_2nd_edge == Direction.UP:
+            index = self._addVerticesOnEdgeUpward(x, y, edge_length, trajectory, index)
+            y += edge_length
+        elif directionof_2nd_edge == Direction.DOWN:
+            index = self._addVerticesOnEdgeDownward(x, y, edge_length, trajectory, index)
+            y -= edge_length
+        elif directionof_2nd_edge == Direction.RIGHT:
+            index = self._addVerticesOnEdgeRightward(x, y, edge_length, trajectory, index)
+            x += edge_length
+        else: # directionof_2nd_edge == Direction.LEFT:
+            index = self._addVerticesOnEdgeLeftward(x, y, edge_length, trajectory, index)
+            x -= edge_length
+
+        # add vertex on 3rd edge
+        if directionof_1st_edge == Direction.UP:
+            index = self._addVerticesOnEdgeDownward(x, y, edge_length, trajectory, index)
+            y -= edge_length
+        elif directionof_1st_edge == Direction.DOWN:
+            index = self._addVerticesOnEdgeUpward(x, y, edge_length, trajectory, index)
+            y += edge_length
+        elif directionof_1st_edge == Direction.RIGHT:
+            index = self._addVerticesOnEdgeLeftward(x, y, edge_length, trajectory, index)
+            x -= edge_length
+        else: # directionof_1st_edge == Direction.LEFT:
+            index = self._addVerticesOnEdgeRightward(x, y, edge_length, trajectory, index)
+            x += edge_length
+
+        # add vertex on 4th edge
+        if directionof_2nd_edge == Direction.UP:
+            index = self._addVerticesOnEdgeDownward(x, y, edge_length, trajectory, index)
+            y -= edge_length
+        elif directionof_2nd_edge == Direction.DOWN:
+            index = self._addVerticesOnEdgeUpward(x, y, edge_length, trajectory, index)
+            y += edge_length
+        elif directionof_2nd_edge == Direction.RIGHT:
+            index = self._addVerticesOnEdgeLeftward(x, y, edge_length, trajectory, index)
+            x -= edge_length
+        else: # directionof_2nd_edge == Direction.LEFT:
+            index = self._addVerticesOnEdgeRightward(x, y, edge_length, trajectory, index)
+            x += edge_length
+
+    def _addVerticesOnEdgeUpward(self, base_x, base_y, edge_length, vertices, index):
+        length = 0
+        while length < edge_length:
+            u, v = vertices[index-1], vertices[index]
+            length += self._distance[(u, v)]
+            self._addVertexToGrid(v, base_x, base_y + length)
+            index += 1
+        return index
+
+    def _addVerticesOnEdgeDownward(self, base_x, base_y, edge_length, vertices, index):
+        length = 0
+        while length < edge_length:
+            u, v = vertices[index-1], vertices[index]
+            length += self._distance[(u, v)]
+            self._addVertexToGrid(v, base_x, base_y - length)
+            index += 1
+        return index
+
+    def _addVerticesOnEdgeRightward(self, base_x, base_y, edge_length, vertices, index):
+        length = 0
+        while length < edge_length:
+            u, v = vertices[index-1], vertices[index]
+            length += self._distance[(u, v)]
+            self._addVertexToGrid(v, base_x + length, base_y)
+            index += 1
+        return index
+
+    def _addVerticesOnEdgeLeftward(self, base_x, base_y, edge_length, vertices, index):
+        length = 0
+        while length < edge_length:
+            u, v = vertices[index-1], vertices[index]
+            length += self._distance[(u, v)]
+            self._addVertexToGrid(v, base_x - length, base_y)
+            index += 1
+        return index
+
+    def _findClosedLoop(self, vertex, edge_length, current_length, depth, trajectory, found_callback):
         if depth == 4:
-            if vertex == trajectory[0]:
-                # found closed loop
+            if vertex != trajectory[0]:
+                return False
 
-                trajectory.append(vertex)
-                print('trajectory: {}'.format(trajectory), file=sys.stderr)
-                index = 1
-
-                # add vertex on 1st edge
-                length = 0
-                while length < edge_length:
-                    u = trajectory[index-1]
-                    v = trajectory[index]
-                    d = self._distance[(u, v)]
-                    length += d
-                    self._addVertexToGrid(v, length, 0)
-                    index += 1
-
-                # add vertex on 2nd edge
-                length = 0
-                while length < edge_length:
-                    u = trajectory[index-1]
-                    v = trajectory[index]
-                    d = self._distance[(u, v)]
-                    length += d
-                    self._addVertexToGrid(v, edge_length, length)
-                    index += 1
-
-                # add vertex on 3rd edge
-                length = 0
-                while length < edge_length:
-                    u = trajectory[index-1]
-                    v = trajectory[index]
-                    d = self._distance[(u, v)]
-                    length += d
-                    self._addVertexToGrid(v, edge_length - length, edge_length)
-                    index += 1
-
-                # add vertex on 4th edge
-                length = 0
-                while length < edge_length:
-                    u = trajectory[index-1]
-                    v = trajectory[index]
-                    d = self._distance[(u, v)]
-                    length += d
-                    self._addVertexToGrid(v, 0, edge_length - length)
-                    index += 1
-                return True
-            return False
+            # found closed loop
+            trajectory.append(vertex)
+            log.debug('trajectory: {}'.format(trajectory))
+            found_callback(edge_length, trajectory)
+            return True
 
         trajectory.append(vertex)
         for edge in self._connectedEdges[vertex]:
@@ -217,10 +368,11 @@ class GridGraph:
 
             result = False
             length = edge.d() + current_length
-            if length < edge_length:
-                result = self._exploreForOrigin(edge.v(), edge.d(), length, depth, trajectory)
-            elif length == edge_length:
-                result = self._exploreForOrigin(edge.v(), edge.d(), 0, depth + 1, trajectory)
+            if length == edge_length:
+                result = self._findClosedLoop(edge.v(), edge.d(), 0, depth + 1, trajectory, found_callback)
+            # TODO
+            #elif length < edge_length:
+            #    result = self._findClosedLoop(edge.v(), edge.d(), length, depth, trajectory, found_callback)
             if result:
                 return True
         trajectory.pop()
@@ -254,21 +406,58 @@ class GridGraph:
             self._1connectionVertex.add(vertex)
 
     def _addVertexToGrid(self, vertex, x, y):
-        assert self._grid[x][y] == None
-        assert self._position.get(vertex) == None
+        #if self._grid[x][y] != None:
+        #    return
+        log.debug('vertex={}, x={}, y={}'.format(vertex, x, y))
+        #assert self._grid[x][y] == None
+        #assert self._position.get(vertex) == None
         self._grid[x][y] = vertex
         self._position[vertex] = (x, y)
+
+    def _getDirection(self, vertex1, vertex2):
+        x1, y1 = self._position[vertex1]
+        x2, y2 = self._position[vertex2]
+        if x1 == x2: # vertical
+            if y1 < y2:
+                direction = Direction.UP
+            else:
+                direction = Direction.DOWN
+        elif y1 == y2: # horizontal
+            if x1 < x2:
+                direction = Direction.RIGHT
+            else:
+                direction = Direction.LEFT
+        else:
+            # not supported input
+            assert False
+        return direction
+
+    def _isHorizontal(self, vertex1, vertex2):
+        direction = self._getDirection(vertex1, vertex2)
+        return direction == Direction.RIGHT or direction == Direction.LEFT
+
+    def _isVertical(self, vertex1, vertex2):
+        direction = self._getDirection(vertex1, vertex2)
+        return direction == Direction.UP or direction == Direction.DOWN
 
     def show(self):
         import matplotlib.pyplot as plt
 
         fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+        #ax.set_xlim(-10, 10)
+        #ax.set_ylim(-10, 10)
         for x, y in self._position.values():
-            print('x={}, y={}'.format(x, y), file=sys.stderr)
             vertex = self._grid[x][y]
-            ax.text(x, y, vertex, va='bottom', ha='left', fontsize=16)
+            ax.text(x, y, vertex, va='bottom', ha='left', fontsize=12)
             ax.plot(x, y, marker='o', color='b')
         plt.show()
+
+class Direction(enum.Enum):
+    UP = enum.auto()
+    DOWN = enum.auto()
+    RIGHT = enum.auto()
+    LEFT = enum.auto()
 
 class Connection4:
     def __init__(self, center, edges):
@@ -413,14 +602,19 @@ class WorkerState_Moving(State):
         sweeped.add(context._current_vertex)
 
         jobFound = False
+        reward_rating_list = list()
+        trajectory_list = list()
         while not pr_queue.empty():
             trajectory = pr_queue.get()
             jobs = context._job_admin.getJobsByVertex(trajectory.getVertices()[-1], context._job_types)
             if len(jobs) > 0:
                 for job in jobs:
                     #log.debug(str(job))
-                    if job.getExpectedReward(context._current_time + trajectory.getDistance(), context._max_workload) > 0:
+                    reward = job.getExpectedReward(context._current_time + trajectory.getDistance(), context._max_workload)
+                    if reward > 0:
                         jobFound = True
+                        reward_rating_list.append(reward / (1 + trajectory.getDistance()))
+                        trajectory_list.append(trajectory)
                         break
 
             for vertex, distance in context._graph.getAdjacentVertices(trajectory.getVertices()[-1]):
@@ -430,13 +624,17 @@ class WorkerState_Moving(State):
                 pr_queue.put(new_trajectory)
                 sweeped.add(vertex)
 
-            if jobFound:
+            #if jobFound:
+            if len(reward_rating_list) > 20:
                 break
 
         if not jobFound:
             #log.debug('sweeped={}'.format(str(sweeped)))
             context._event_queue.append(WorkerEvent.NO_JOB)
             return
+
+        best = reward_rating_list.index(max(reward_rating_list))
+        trajectory = trajectory_list[best]
 
         if trajectory.getDistance() == 0:
             context._event_queue.append(WorkerEvent.MOVED)
@@ -693,8 +891,6 @@ class Environment:
             edge = Edge(int(u), int(v), int(d))
             edges.append(edge)
         self._graph = Graph(edges)
-        self._gridGraph = GridGraph(edges)
-        exit()
 
         # Worker
         Nworker, = input().split()
