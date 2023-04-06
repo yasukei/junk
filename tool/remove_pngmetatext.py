1eff2286b57d437ebad635e35044796049a2823c
Kei Yasuda <yasu_kei@icloud.com>
2023-04-06 21:24:24 +0900

    updated

--- tool/remove_pngmetatext.py
+++ tool/remove_pngmetatext.py
@@ -1,16 +1,30 @@
-import sys
+import os
+import shutil
+from pathlib import Path
 
 from PIL.PngImagePlugin import PngImageFile
 
 def main():
-    if len(sys.argv) <= 1:
-        print('Error: specify input_file')
-        return
+    # Variables
+    dir_path = './'
+    target_extension = '.png'
 
-    file_name = sys.argv[1]
-    image = PngImageFile(file_name)
-    image.text.clear()
-    image.save(file_name)
+    # Get a list of files
+    files_and_dirs = os.listdir(dir_path)
+    files = [elem for elem in files_and_dirs if os.path.isfile(os.path.join(dir_path, elem))]
+
+    # Remove PngInfo
+    for file in files:
+        name, extension = os.path.splitext(file)
+        if extension != target_extension:
+            continue
+
+        print(f'removing pnginfo of {file}')
+        image = PngImageFile(file)
+        image.text.clear()
+        image.info.clear()
+        image.save(file)
+    print('Done.')
 
 if __name__ == '__main__':
     main()
